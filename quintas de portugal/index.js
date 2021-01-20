3f471fb30b0fce26c8b85a25dea14c85b490f5bd
gabrielnuno1206 <77217906+gabrielnuno1206@users.noreply.github.com>
2021-01-20 09:48:29 -0300

    Add files via upload

---
+++ quintas de portugal/index.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+function index(){
+    return <div>
+        <h1>sobre</h1>
+        
+        <Link href="/sobre">
+        <a> Acessar pagina sobre </a>
+         </Link>
+</div>
+export default[icomunicado.html]
+        
+
+
+
+}
---
+++ quintas de portugal/sobre.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function index(){
+    return <div>
+        <h1>sobre</h1>
+        
+        <Link href="/index">
+        <a> Acessar pagina index </a>
+         </Link>
+</div>
+export default[index.html]
+}     
